9a8a6d521f58f6d6e615c790c1279ab6c6c91aad
rduboveckij <andrus.god@gmail.com>
2014-04-09 00:02:44 +0300

    Init commit

---
+++ src/main/java/com/rduboveckij/edu/patterns/gof/structural/AbstractFactory.java
@@ -0,0 +1,34 @@
+package com.rduboveckij.edu.patterns.gof.structural;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * author: Ruslan Duboveckij
+ */
+public class AbstractFactory {
+    public static interface BaseVehicle {
+        String getMessage();
+    }
+
+    public static class VehicleFactory {
+        private final static Map<String, BaseVehicle> vehicles = new HashMap<>();
+
+        static {
+            vehicles.put("car", () -> "I am car");
+            vehicles.put("airplane", () -> "I am airplane");
+        }
+
+        public static BaseVehicle getVehicle(String name) throws Exception {
+            final BaseVehicle vehicle = vehicles.get(name);
+            if (vehicle == null) throw new Exception("Not found vehicle with name: " + name);
+            return vehicle;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(VehicleFactory.getVehicle("car").getMessage());
+        System.out.println(VehicleFactory.getVehicle("airplane").getMessage());
+        System.out.println(VehicleFactory.getVehicle("not found name").getMessage());
+    }
+}
